e4c10bdba79ceacbcb367d563f30f11eb5043e89
sirSlappz <tamestranger43@gmail.com>
2022-07-13 12:09:59 -0500

    fixed server name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dad-joke'));
+app.use(express.static(__dirname + '/dist/DadJoke'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/dad-joke/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/DadJoke/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
